58f97d9692e39a0cccd8aa939385e171cf2ebfa1
AregawiY <Yosefyabe@gmail.com>
2016-08-17 14:33:27 -0400

    AsyncExample
    Sample project to show how async , wait works

---
+++ AsyncExample1/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace AsyncExample1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTestAsync = new System.Windows.Forms.Button();
+            this.txtMessageFirst = new System.Windows.Forms.TextBox();
+            this.txtMessageLast = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnTestAsync
+            // 
+            this.btnTestAsync.Location = new System.Drawing.Point(60, 186);
+            this.btnTestAsync.Name = "btnTestAsync";
+            this.btnTestAsync.Size = new System.Drawing.Size(160, 40);
+            this.btnTestAsync.TabIndex = 0;
+            this.btnTestAsync.Text = "TestAsync";
+            this.btnTestAsync.UseVisualStyleBackColor = true;
+            this.btnTestAsync.Click += new System.EventHandler(this.btnTestAsync_Click);
+            // 
+            // txtMessageFirst
+            // 
+            this.txtMessageFirst.Location = new System.Drawing.Point(31, 34);
+            this.txtMessageFirst.Name = "txtMessageFirst";
+            this.txtMessageFirst.Size = new System.Drawing.Size(545, 22);
+            this.txtMessageFirst.TabIndex = 1;
+            // 
+            // txtMessageLast
+            // 
+            this.txtMessageLast.Location = new System.Drawing.Point(31, 107);
+            this.txtMessageLast.Name = "txtMessageLast";
+            this.txtMessageLast.Size = new System.Drawing.Size(545, 22);
+            this.txtMessageLast.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(612, 253);
+            this.Controls.Add(this.txtMessageLast);
+            this.Controls.Add(this.txtMessageFirst);
+            this.Controls.Add(this.btnTestAsync);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTestAsync;
+        private System.Windows.Forms.TextBox txtMessageFirst;
+        private System.Windows.Forms.TextBox txtMessageLast;
+    }
+}
+
---
+++ AsyncExample1/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// Add a using directive and a reference for System.Net.Http;
+using System.Net.Http;
+
+namespace AsyncExample1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnTestAsync_Click(object sender, EventArgs e)
+        {
+            int contentLength = await testAsyncMessage();
+            txtMessageFirst.Text +=
+                String.Format("\r\nLength of the downloaded string: {0}.\r\n", contentLength);
+        }
+
+        async Task<int> testAsyncMessage()
+        {
+            // You need to add a reference to System.Net.Http to declare client.
+            HttpClient client = new HttpClient();
+
+            // GetStringAsync returns a Task<string>. That means that when you await the
+            // task you'll get a string (urlContents).
+            Task<string> getStringTask = client.GetStringAsync("http://msdn.microsoft.com");
+
+            // You can do work here that doesn't rely on the string from GetStringAsync.
+            DoIndependentWork();
+
+            // The await operator suspends AccessTheWebAsync.
+            //  - AccessTheWebAsync can't continue until getStringTask is complete.
+            //  - Meanwhile, control returns to the caller of AccessTheWebAsync.
+            //  - Control resumes here when getStringTask is complete. 
+            //  - The await operator then retrieves the string result from getStringTask.
+            string urlContents = await getStringTask;
+
+            // The return statement specifies an integer result.
+            // Any methods that are awaiting AccessTheWebAsync retrieve the length value.
+            return urlContents.Length;
+        }
+        void DoIndependentWork()
+        {
+             txtMessageLast.Text += "Working . . . . . . .\r\n";
+        }
+    }
+}
+
